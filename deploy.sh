392bf64b578eb90dbc74df4e0d41483495649234
ilhooq <contact@sphilip.com>
2021-01-23 03:13:15 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+bundle exec jekyll build
+
+# navigate into the build output directory
+cd _site
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:ilhooq/piko-framework/piko-framework.github.io.git master:main
+
+cd -
+
+
